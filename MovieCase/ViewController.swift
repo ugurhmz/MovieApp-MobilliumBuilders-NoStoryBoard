9ec23305b5a3bebf42881995ba9ba2a4f2671a0a
Ugurhamzaoglu67 <craxx67@gmail.com>
2022-05-11 16:48:11 +0300

    init

---
+++ MovieCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieCase
+//
+//  Created by ugur-pc on 11.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
