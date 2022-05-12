4384d9f5d5cf2bd6ad5f080acc769276dafa6199
Ugurhamzaoglu67 <craxx67@gmail.com>
2022-05-12 11:50:33 +0300

    HomeTopSliderCell edited

--- MovieCase/UIComponents/HomeTopCells/HomeTopCell.swift
+++ MovieCase/UIComponents/HomeTopCells/HomeTopCell.swift
@@ -6,15 +6,121 @@
 //
 
 import UIKit
+import MobilliumBuilders
 
 class HomeTopCell: UICollectionViewCell {
     static var identifier = "HomeTopCell"
     
+    private let topCellCollectionView = UICollectionViewBuilder()
+        .scrollDirection(.horizontal)
+        .showsVerticalScrollIndicator(false)
+        .showsHorizontalScrollIndicator(false)
+        .isPagingEnabled(true)
+        .registerCell(HomeTopSliderCell.self,
+                      reuseIdentifier: HomeTopSliderCell.identifier)
+        .build()
+        
+    
+    private let pageControl = UIPageControlBuilder()
+        .currentPageIndicatorTintColor(.white)
+        .pageIndicatorTintColor(.darkGray)
+        .numberOfPages(4)
+        .build()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        addSubviews()
+        setConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("not imp")
     }
 }
+
+// MARK -
+extension HomeTopCell {
+    private func addSubviews(){
+        addSubview(topCellCollectionView)
+        addSubview(pageControl)
+        topCellCollectionView.dataSource = self
+        topCellCollectionView.delegate = self
+        
+        self.pageControl.numberOfPages = 5
+    }
+    
+    private func setConstraints(){
+        topCellCollectionView.fillSuperview()
+        
+        pageControl.centerXInSuperview()
+        pageControl.anchor(top: nil,
+                           leading: nil,
+                           bottom: bottomAnchor,
+                           trailing: nil,
+                           padding: .init(top: 0, left: 0, bottom: 10, right: 0))
+    }
+}
+
+
+//MARK: - UICollectionViewDelegate, UICollectionViewDataSource
+extension HomeTopCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+            self.pageControl.currentPage = indexPath.row
+     }
+        
+        
+    // paging
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView,
+                                   withVelocity velocity: CGPoint,
+                                   targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let x = targetContentOffset.pointee.x
+        pageControl.currentPage = Int(x / topCellCollectionView.frame.width)
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let sliderCell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeTopSliderCell.identifier, for: indexPath) as? HomeTopSliderCell else {
+            return UICollectionViewCell()
+        }
+        sliderCell.backgroundColor = UIColor(hue: drand48(),
+                                             saturation: 1,
+                                             brightness: 1,
+                                             alpha: 1)
+        return sliderCell
+    }
+    
+}
+
+//MARK: -
+extension HomeTopCell: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: topCellCollectionView.frame.width,
+                      height: topCellCollectionView.frame.height)
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+       
+        return .zero
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+}
+
---
+++ MovieCase/UIComponents/HomeTopCells/HomeTopSliderCell.swift
@@ -0,0 +1,88 @@
+//
+//  File.swift
+//  MovieCase
+//
+//  Created by ugur-pc on 12.05.2022.
+//
+
+import UIKit
+import MobilliumBuilders
+
+class HomeTopSliderCell: UICollectionViewCell {
+    static var identifier = "HomeTopSliderCell"
+    
+    private let movieImgView = UIImageViewBuilder()
+        .backgroundColor(.clear)
+        .contentMode(.scaleToFill)
+        .image(UIImage(named:"a5") ?? UIImage())
+        .cornerRadius(10)
+        .clipsToBounds(true)
+        .build()
+    
+ 
+    private let transparentView = UIViewBuilder()
+        .backgroundColor(.black.withAlphaComponent(0.3))
+        .build()
+    
+    
+    private let titleLbl = UILabelBuilder()
+        .font(.systemFont(ofSize: 21, weight: .bold))
+        .text("Spider Man 2013")
+        .textColor(.white)
+        .numberOfLines(2)
+        .build()
+    
+    
+    private let definitionLbl = UILabelBuilder()
+        .font(.systemFont(ofSize: 15, weight: .medium))
+        .textColor(.white)
+        .text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also th")
+        .numberOfLines(2)
+        .build()
+    
+    private let labelStackView = UIStackViewBuilder()
+        .axis(.vertical)
+        .distribution(.fillProportionally)
+        .spacing(10)
+        .build()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubviews()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("not imp")
+    }
+}
+
+extension HomeTopSliderCell {
+    
+    private func addSubviews() {
+        addSubview(movieImgView)
+        addSubview(transparentView)
+        addSubview(labelStackView)
+        
+        [titleLbl,definitionLbl].forEach{ labelStackView.addArrangedSubview($0)}
+    }
+    
+    private func setConstraints(){
+        movieImgView.anchor(top: contentView.topAnchor,
+                                     leading: contentView.leadingAnchor,
+                                     bottom: contentView.bottomAnchor,
+                                     trailing: contentView.trailingAnchor)
+               
+        transparentView.anchor(top: safeAreaLayoutGuide.topAnchor,
+                                      leading: contentView.leadingAnchor,
+                                      bottom: contentView.bottomAnchor,
+                                      trailing: contentView.trailingAnchor)
+               
+        labelStackView.anchor(top: nil,
+                                      leading: contentView.leadingAnchor,
+                                      bottom: contentView.bottomAnchor,
+                                      trailing: contentView.trailingAnchor,
+                                      padding: .init(top: 0, left: 15, bottom: 70, right: 15))
+    }
+}
